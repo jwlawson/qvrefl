fff81a9330a276d85cb184d5d68bfd457e71ec71
John Lawson <john@jwlawson.co.uk>
2016-07-12 11:25:05 +0100

    Adds implementation checking compatible cartans

--- include/compatible_cartan.h
+++ include/compatible_cartan.h
@@ -12,9 +12,26 @@
 namespace refl {
 class CompatibleCartan {
 public:
+	template<class mat>
 	bool
-	operator()(cluster::QuiverMatrix const& lhs, arma::mat const& rhs) const;
+	operator()(cluster::QuiverMatrix const& lhs, mat const& rhs) const;
 };
+template<class mat>
+bool
+CompatibleCartan::operator()(cluster::QuiverMatrix const& lhs,
+		mat const& rhs) const {
+	bool result = static_cast<uint32_t>(lhs.num_cols()) == rhs.n_cols
+		&& static_cast<uint32_t>(lhs.num_rows()) == rhs.n_rows;
+	/* As the two types of matrix are arranged differently in memory, we cannot
+	 * use quick comparisons, but have to go through each value separately. */
+	for(uint32_t row = 0; result && row < rhs.n_rows; ++row) {
+		for(uint32_t col = 0; result && col < rhs.n_cols; ++col) {
+			if(row == col) continue;
+			result = std::abs(lhs.get(row,col)) == std::abs(rhs(row, col));
+		}
+	}
+	return result;
+}
 }
 #endif
 
--- test/compatible_cartan_test.cc
+++ test/compatible_cartan_test.cc
@@ -28,12 +28,12 @@ TEST(CC, Diff1) {
 	arma::mat a = { { 2, -1, 1 }, { -1, 2, 1 }, { 1, 1, 2 } };
 	cluster::QuiverMatrix q("{ { 0 2 1 } { -2 0 1 } { -1 -1 0 } }");
 	CompatibleCartan cc;
-	EXPECT_TRUE(cc(q, a));
+	EXPECT_FALSE(cc(q, a));
 }
 TEST(CC, Diff2) {
 	arma::mat a = { { 2, -1, 1 }, { -1, 2, 1 }, { 1, 1, 2 } };
 	cluster::QuiverMatrix q("{ { 0 1 2 } { -1 0 1 } { -2 -1 0 } }");
 	CompatibleCartan cc;
-	EXPECT_TRUE(cc(q, a));
+	EXPECT_FALSE(cc(q, a));
 }
 }
