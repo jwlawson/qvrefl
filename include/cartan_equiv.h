1bac5e6b8dcd2222226cb51ae9f1441586581e73
John Lawson <john@jwlawson.co.uk>
2016-09-19 12:38:53 +0100

    Adds check for whether cartan matrices are equivalent.
    As the cartan matrix is a gram matrix, and we only care about the hyperplanes
    which the are normal to the vectors, multiplying any vector by -1 makes no
    difference. This would multiply the row and column in the gram matrix by -1
    simulateously. Hence we need to consider two matrices equivalent if you can
    go from one to the other by simultaneously mutliplying rows and columns by
    -1. This is the check in cartan_equiv.

---
+++ include/cartan_equiv.h
@@ -0,0 +1,71 @@
+/**
+ * cartan_equiv.h
+ * Copyright 2016 John Lawson
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#pragma once
+#ifndef REFL_CARTAN_MUTATOR_H__
+#define REFL_CARTAN_MUTATOR_H__
+
+#include <armadillo>
+#include <boost/dynamic_bitset.hpp>
+
+namespace refl {
+class CartanEquiv {
+public:
+  template <class elem_t>
+  bool operator()(arma::Mat<elem_t> const &lhs, arma::Mat<elem_t> const &rhs);
+
+private:
+  boost::dynamic_bitset<> flipped;
+};
+
+template <class elem_t>
+bool CartanEquiv::operator()(arma::Mat<elem_t> const &lhs,
+                             arma::Mat<elem_t> const &rhs) {
+  size_t const nrows = lhs.n_rows;
+  size_t const ncols = lhs.n_cols;
+  bool result = nrows == rhs.n_rows && ncols == rhs.n_cols;
+  auto fl_ind = [nrows](size_t row, size_t col) { return col * nrows + row; };
+  flipped.reset();
+  flipped.resize(nrows * ncols, false);
+  size_t col = 0;
+  for (size_t row = col + 1; result && row < nrows; ++row) {
+    bool already_flipped = flipped.test(fl_ind(row, col));
+    if (lhs(row, col) == -rhs(row, col)) {
+      // If already flipped, then this has been fixed.
+      if (!already_flipped) {
+        for (size_t second = col + 1; second < row; ++second) {
+          flipped.flip(fl_ind(row, second));
+        }
+        for (size_t first = row + 1; first < ncols; ++first) {
+          flipped.flip(fl_ind(first, row));
+        }
+      }
+    } else {
+      result = !already_flipped && lhs(row, col) == rhs(row, col);
+    }
+  }
+  for (col = 1; result && col < ncols; ++col) {
+    for (size_t row = col + 1; result && row < nrows; ++row) {
+      bool already_flipped = flipped.test(fl_ind(row, col));
+      result = (already_flipped && lhs(row, col) == -rhs(row, col)) ||
+               (!already_flipped && lhs(row, col) == rhs(row, col));
+    }
+  }
+
+  return result;
+}
+}
+#endif
---
+++ test/cartan_equiv_test.cc
@@ -0,0 +1,114 @@
+/**
+ * cartan_equiv_test.cc
+ * Copyright 2016 John Lawson
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#include "cartan_equiv.h"
+
+#include "gtest/gtest.h"
+
+namespace refl {
+TEST(CartanEquiv, Same) {
+  arma::Mat<int> a{{2, 1, 1, 1}, {1, 2, 1, 1}, {1, 1, 2, 1}, {1, 1, 1, 2}};
+  CartanEquiv equiv;
+
+  EXPECT_TRUE(equiv(a, a));
+
+  arma::Mat<int> b{{2, 1, 1, 1}, {1, 2, 1, 1}, {1, 1, 2, 1}, {1, 1, 1, 2}};
+  EXPECT_TRUE(equiv(a, b));
+  EXPECT_TRUE(equiv(b, a));
+}
+TEST(CartanEquiv, OneSwitch) {
+  arma::Mat<int> a{{2, 1, 1, 1}, {1, 2, 1, 1}, {1, 1, 2, 1}, {1, 1, 1, 2}};
+  arma::Mat<int> b{
+      {2, -1, -1, -1}, {-1, 2, 1, 1}, {-1, 1, 2, 1}, {-1, 1, 1, 2}};
+  arma::Mat<int> c{
+      {2, -1, 1, 1}, {-1, 2, -1, -1}, {1, -1, 2, 1}, {1, -1, 1, 2}};
+  arma::Mat<int> d{
+      {2, 1, -1, 1}, {1, 2, 1, -1}, {-1, -1, 2, -1}, {1, 1, -1, 2}};
+  arma::Mat<int> e{
+      {2, 1, 1, -1}, {1, 2, 1, -1}, {1, 1, 2, -1}, {-1, -1, -1, 2}};
+
+  CartanEquiv equiv;
+  EXPECT_TRUE(equiv(a, b));
+  EXPECT_TRUE(equiv(a, c));
+  EXPECT_TRUE(equiv(a, d));
+  EXPECT_TRUE(equiv(a, e));
+
+  EXPECT_TRUE(equiv(b, a));
+  EXPECT_TRUE(equiv(c, a));
+  EXPECT_TRUE(equiv(d, a));
+  EXPECT_TRUE(equiv(e, a));
+}
+TEST(CartanEquiv, TwoSwitch) {
+  arma::Mat<int> a{{2, 1, 1, 1}, {1, 2, 1, 1}, {1, 1, 2, 1}, {1, 1, 1, 2}};
+  arma::Mat<int> b{
+      {2, -1, 1, -1}, {-1, 2, -1, 1}, {1, -1, 2, -1}, {-1, 1, -1, 2}};
+  arma::Mat<int> c{
+      {2, -1, -1, 1}, {-1, 2, 1, -1}, {-1, 1, 2, -1}, {1, -1, -1, 2}};
+  arma::Mat<int> d{
+      {2, 1, -1, -1}, {1, 2, -1, -1}, {-1, -1, 2, 1}, {-1, -1, 1, 2}};
+
+  CartanEquiv equiv;
+  EXPECT_TRUE(equiv(a, b));
+  EXPECT_TRUE(equiv(a, c));
+  EXPECT_TRUE(equiv(a, d));
+
+  EXPECT_TRUE(equiv(b, a));
+  EXPECT_TRUE(equiv(c, a));
+  EXPECT_TRUE(equiv(d, a));
+}
+TEST(CartanEquiv, NotSignEquiv) {
+  arma::Mat<int> a{{2, 1, 1, 1}, {1, 2, 1, 1}, {1, 1, 2, 1}, {1, 1, 1, 2}};
+  arma::Mat<int> b{{2, 1, -1, -1}, {1, 2, 1, 1}, {-1, 1, 2, 1}, {-1, 1, 1, 2}};
+  arma::Mat<int> c{
+      {2, -1, 1, -1}, {-1, 2, -1, -1}, {1, -1, 2, 1}, {-1, -1, 1, 2}};
+  arma::Mat<int> d{{2, 1, -1, 1}, {1, 2, 1, 1}, {-1, 1, 2, -1}, {1, 1, -1, 2}};
+  arma::Mat<int> e{{2, 1, 1, 1}, {1, 2, 1, -1}, {1, 1, 2, -1}, {1, -1, -1, 2}};
+
+  CartanEquiv equiv;
+  EXPECT_FALSE(equiv(a, b));
+  EXPECT_FALSE(equiv(a, c));
+  EXPECT_FALSE(equiv(a, d));
+  EXPECT_FALSE(equiv(a, e));
+
+  EXPECT_FALSE(equiv(b, a));
+  EXPECT_FALSE(equiv(c, a));
+  EXPECT_FALSE(equiv(d, a));
+  EXPECT_FALSE(equiv(e, a));
+}
+TEST(CartanEquiv, TrickySignNotEquiv) {
+  arma::Mat<int> a{{2, 1, 1, 1}, {1, 2, 1, 1}, {1, 1, 2, 1}, {1, 1, 1, 2}};
+  arma::Mat<int> b{
+      {2, 1, -1, -1}, {1, 2, 1, -1}, {-1, 1, 2, 1}, {-1, -1, 1, 2}};
+
+  CartanEquiv equiv;
+  EXPECT_FALSE(equiv(a, b));
+  EXPECT_FALSE(equiv(b, a));
+}
+TEST(CartanEquiv, NotEqual) {
+  arma::Mat<int> a{{2, 1, 1, 1}, {1, 2, 1, 1}, {1, 1, 2, 1}, {1, 1, 1, 2}};
+  arma::Mat<int> b{{2, 4, -3, 1}, {-3, 2, 2, 1}, {-1, 1, 2, 1}, {-1, 1, 1, 2}};
+  CartanEquiv equiv;
+  EXPECT_FALSE(equiv(a, b));
+  EXPECT_FALSE(equiv(b, a));
+}
+TEST(CartanEquiv, DifferentSize) {
+  arma::Mat<int> a{{2, 1, 1}, {1, 2, 1}, {1, 1, 2}};
+  arma::Mat<int> b{{2, 1, 1, 1}, {1, 2, 1, 1}, {1, 1, 2, 1}, {1, 1, 1, 2}};
+  CartanEquiv equiv;
+  EXPECT_FALSE(equiv(a, b));
+  EXPECT_FALSE(equiv(b, a));
+}
+}
