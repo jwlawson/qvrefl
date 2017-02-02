585ac517710ee6f952b6d5f7398f96b0e1c83598
John Lawson <john@jwlawson.co.uk>
2017-02-02 12:54:30 +0000

    Adds exchange graph of cartan matrices.

---
+++ include/cartan_exchange_graph.h
@@ -0,0 +1,51 @@
+#pragma once
+#ifndef REFL_CARTAN_EXCHANGE_GRAPH_H__
+#define REFL_CARTAN_EXCHANGE_GRAPH_H__
+
+#include "qv/equiv_quiver_matrix.h"
+#include "qv/equivalence_checker.h"
+#include "qv/template_exchange_graph.h"
+
+#include <armadillo>
+
+namespace refl {
+namespace cartan_exchange {
+struct CartanQuiver {
+  cluster::EquivQuiverMatrix quiver;
+  arma::Mat<int> cartan;
+  bool fully_compatible;
+};
+struct Mutator {
+  void operator()(CartanQuiver const& initial,
+                  size_t k,
+                  CartanQuiver& output) const;
+};
+struct Equiv {
+  bool operator()(CartanQuiver const* const lhs,
+                  CartanQuiver const* const rhs) const;
+};
+struct Equal {
+  bool operator()(CartanQuiver const* const lhs,
+                  CartanQuiver const* const rhs) const;
+};
+struct Hash {
+  size_t operator()(CartanQuiver const* const quiver) const;
+};
+struct NewInstance {
+  CartanQuiver* operator()(size_t const size) const;
+};
+struct DontMutateNonCompatible {
+  bool operator()(CartanQuiver const* const mptr, int /*vertex*/) const;
+};
+using CartanVertex = cluster::exchange_graph::detail::
+    VertexInfo<CartanQuiver, Hash, Equiv, Equal, Mutator, NewInstance>;
+using CartanGraphInfo = cluster::exchange_graph::detail::GraphInfo<
+    DontMutateNonCompatible,
+    cluster::exchange_graph::detail::NeverStop>;
+}
+typedef cluster::exchange_graph::Graph<cartan_exchange::CartanVertex,
+                                       cartan_exchange::CartanGraphInfo>
+    CartanExchangeGraph;
+}
+
+#endif
---
+++ src/cartan_exchange_graph.cc
@@ -0,0 +1,45 @@
+#include "cartan_exchange_graph.h"
+
+#include "cartan_mutator.h"
+#include "fully_compatible_check.h"
+#include "permuted_cartan_equiv.h"
+
+namespace refl {
+namespace cartan_exchange {
+void Mutator::operator()(CartanQuiver const& initial,
+                         size_t k,
+                         CartanQuiver& output) const {
+  initial.quiver.mutate(k, output.quiver);
+  CartanMutator cmut(initial.quiver);
+  cmut(initial.cartan, k, output.cartan);
+  FixedQuiverFullyCompatibleCheck comp(output.quiver);
+  output.fully_compatible = comp(output.cartan);
+}
+bool Equiv::operator()(CartanQuiver const* const lhs,
+                       CartanQuiver const* const rhs) const {
+  auto check = cluster::EquivalenceChecker::Get(lhs->quiver.num_rows());
+  bool result = check->are_equivalent(lhs->quiver, rhs->quiver);
+  if (result) {
+    auto permutation = check->last_row_map();
+    PermutedCartanEquiv equiv;
+    result = equiv(lhs->cartan, rhs->cartan, permutation);
+  }
+  return result;
+}
+bool Equal::operator()(CartanQuiver const* const lhs,
+                       CartanQuiver const* const rhs) const {
+  return cluster::IntMatrix::are_equal(lhs->quiver, rhs->quiver);
+}
+size_t Hash::operator()(CartanQuiver const* const quiver) const {
+  return quiver->quiver.hash();
+}
+CartanQuiver* NewInstance::operator()(size_t const size) const {
+  return new CartanQuiver{cluster::EquivQuiverMatrix(size, size),
+                          arma::Mat<int>(size, size), false};
+}
+bool DontMutateNonCompatible::operator()(CartanQuiver const* const mptr,
+                                         int /*vertex*/) const {
+  return mptr->fully_compatible;
+}
+}
+}
